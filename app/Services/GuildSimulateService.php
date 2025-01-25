dd018f922b8c44cc96094539c8e230f7c0bf73a3
EduardoAtene <eduardoatenesilvae@gmail.com>
2025-01-25 17:36:26 -0300

    ✨ Feat: Add rule too class. Initial algirtmo pq ta mt ruim isso a forma que fiz. Mas é o inicial

--- app/Services/GuildSimulateService.php
+++ app/Services/GuildSimulateService.php
@@ -41,23 +41,14 @@ class GuildSimulateService
                 'guild_name' => $guild['name'],
                 'qnt_players' => $guild['player_count'],
                 'players' => [],
+                'missing_classes' => [],
                 'total_xp' => 0,
             ];
         }, $guilds);
 
         $players = $playerAssociate->toArray();
-        $totalPlayers = count($players);
-        $currentPlayerIndex = 0;
 
-        foreach ($data as $key => $guild) {
-            $playerCount = $guild['qnt_players'];
-            $guildPlayers = array_slice($players, $currentPlayerIndex, $playerCount);
-            $data[$key]['players'] = $guildPlayers;
-    
-            $data[$key]['total_xp'] = array_sum(array_column($guildPlayers, 'xp'));
-    
-            $currentPlayerIndex += $playerCount;
-        }
+        $this->distributeClassesAmongGuilds($players, $data);
 
         return $data;
     }
@@ -87,4 +78,62 @@ class GuildSimulateService
             throw new ValidateException("A sessão de RPG com o ID {$rpgSession->id} já foi fechada.");
         }
     }
+
+
+    private function distributeClassesAmongGuilds(array $players, array &$guilds): void
+    {
+        $classes = [
+            1 => 'Guerreiro',
+            2 => 'Mago',
+            3 => 'Arqueiro',
+            4 => 'Clérigo',
+        ];
+    
+        $playersGroupedByClass = collect($players)->groupBy(fn($player) => $player['class']['id'])->toArray();
+    
+        $suportGroup = $playersGroupedByClass[4] ?? [];
+        $warriorGroup = $playersGroupedByClass[1] ?? [];
+        $rangedGroup = array_merge(
+            $playersGroupedByClass[2] ?? [],
+            $playersGroupedByClass[3] ?? []
+        );
+    
+        foreach ($guilds as &$guild) {
+            $guild['players'] = [];
+            $guild['missing_classes'] = [];
+    
+            if (!empty($suportGroup)) {
+                $guild['players'][] = array_shift($suportGroup);
+            } else {
+                $guild['missing_classes'][] = $classes[4];
+            }
+    
+            if (!empty($warriorGroup)) {
+                $guild['players'][] = array_shift($warriorGroup);
+            } else {
+                $guild['missing_classes'][] = $classes[1];
+            }
+    
+            if (!empty($rangedGroup)) {
+                $guild['players'][] = array_shift($rangedGroup);
+            } else {
+                $guild['missing_classes'][] = 'Ataque à Distância (' . $classes[2] . ' ou ' . $classes[3] . ')';
+            }
+    
+            while (count($guild['players']) < $guild['qnt_players']) {
+                if (!empty($suportGroup)) {
+                    $guild['players'][] = array_shift($suportGroup);
+                } elseif (!empty($warriorGroup)) {
+                    $guild['players'][] = array_shift($warriorGroup);
+                } elseif (!empty($rangedGroup)) {
+                    $guild['players'][] = array_shift($rangedGroup);
+                } else {
+                    break;
+                }
+            }
+    
+            $guild['total_xp'] = array_sum(array_column($guild['players'], 'xp'));
+        }
+    }
+    
 }
